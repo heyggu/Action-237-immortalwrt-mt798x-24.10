64726fbe3b7c6d30596c544ff8b4c93fc8ff7f61
heyggu <heyggu@gmail.com>
2025-09-06 17:22:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,22 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+# natmapt
+git clone --depth 1 --branch master --single-branch https://github.com/muink/luci-app-natmapt.git package/luci-app-natmapt
+git clone --depth 1 --branch master --single-branch https://github.com/muink/openwrt-natmapt.git package/natmapt
+git clone --depth 1 --branch master --single-branch https://github.com/muink/openwrt-stuntman.git package/stuntman
+# easytier
+git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git package/package-easytier
+#tailscale
+git clone https://github.com/asvow/luci-app-tailscale.git package/luci-app-tailscale
+#fakehttp
+git clone https://github.com/yingziwu/openwrt-fakehttp.git package/fakehttp
+git clone https://github.com/yingziwu/luci-app-fakehttp.git package/luci-app-fakehttp
+#fakesip
+git clone https://github.com/blueberry-pie-11/openwrt-fakesip package/fakesip
+git clone https://github.com/blueberry-pie-11/luci-app-fakesip package/luci-app-fakesip
+#mihomo
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/openwrt-nikki
+#homeproxy
+git clone https://github.com/VIKINGYFY/homeproxy.git package/luci-app-homeproxy
+#git clone https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
