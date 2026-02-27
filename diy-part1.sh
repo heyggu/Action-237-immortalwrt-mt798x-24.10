83b18b1f9ac6c1cb775e3af3b4dc3c64e8a5bead
heyggu <heyggu@gmail.com>
2026-02-27 19:16:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -27,7 +27,7 @@ git clone --depth 1 --branch master --single-branch https://github.com/muink/ope
 git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git package/package-easytier
 #tailscale
 #git clone https://github.com/asvow/luci-app-tailscale.git package/luci-app-tailscale
-#git clone https://github.com/GuNanOvO/openwrt-tailscale.git package/tailscale
+git clone https://github.com/GuNanOvO/openwrt-tailscale.git package/tailscale
 git clone https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community.git package/luci-app-tailscale-community
 
 #fakehttp
